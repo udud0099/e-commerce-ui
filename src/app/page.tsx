b43f224664e9b8feab2b3563fccd4fe4cbbab36a
udud0099 <udud0099@gmail.com>
2025-08-12 10:31:28 +0545

    start the project - https://www.youtube.com/watch?v=6dvYioHX328

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+  return <div className="">Homepage -ready</div>;
+};
 
-export default Homepage
+export default Homepage;
