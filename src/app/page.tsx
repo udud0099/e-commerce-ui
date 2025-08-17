8052d75ff0e72e76fb6d6180f9357ef85dd7afb0
udud0099 <udud0099@gmail.com>
2025-08-17 15:08:13 +0545

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
-const Homepage = () => {
+const Homepage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) => {
+  const category = (await searchParams).category;
+
   return (
     <div className="">
       <div className="relative aspect-[3/1] mb-12">
         <Image src="/featured.png" alt="Featured Product" fill />
       </div>
       <ProductList
-      //  category={category}
-      // params="homepage"
+        category={category}
+        //  params="homepage"
       />
       <h1>te</h1>
       <h1>te</h1>
